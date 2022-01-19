a74edb8693c6d047cf0b384f19ad6ac6c1074adc
KushGabani <kushgabz2687@gmail.com>
2022-01-19 16:43:24 +0530

    build: basic skeleton

---
+++ krypt-client/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as NavBar } from "./NavBar"
+export { default as Footer } from "./Footer"
+export { default as Services } from "./Services"
+export { default as Transcations } from "./Transcations"
+export { default as Welcome } from "./Welcome"
